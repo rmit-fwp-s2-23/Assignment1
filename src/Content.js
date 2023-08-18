7adf3a8af4a38e81e84d9616319026923de0c865
AhmadKayyali <ahmadkayy@gmail.com>
2023-08-18 13:07:26 +1000

    updated content, search bar now works as intended

--- src/Content.js
+++ src/Content.js
@@ -7,51 +7,58 @@ import "./App.css";
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 function Content() {
-    const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const [movies, setMovies] = useState([]);
-  
-    useEffect(() => {
-      searchMovies(searchTerm);
-    }, [searchTerm]);
-  
-    const searchMovies = async (title) => {
-      const response = await fetch(`${API_URL}&s=${title}`);
-      const data = await response.json();
-  
-      setMovies(data.Search);
-    };
+  useEffect(() => {
+    searchMovies("Shrek");
+  }, []);
 
-    return (
+  const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+
+    if (searchTerm) {
+        searchMovies(searchTerm);
+    }}, [searchTerm]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  return (
     <div className="app">
-    <div className="search">
-    <input
-        value={searchTerm}
+      <div className="search">
+        <input
+          name="inputMovie"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          className="placeholder"
+          placeholder="Search for movies"
+        />
+        <img
+          className="./public/searchimg"
+          src={"search.png"}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
 
-        onChange={(event) => setSearchTerm(event.target.value)}
-         className ="placeholder" placeholder="Search for movies" /> 
-    <img
-        className ="./public/searchimg"
-        src={"search.png"}
-        alt="search"
-        onClick={() => searchMovies(searchTerm)}
-    />
+      {movies ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <br />
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
-
-    {movies?.length > 0 ? (
-    <div className="container">
-        {movies.map((movie) => (
-        <MovieCard movie={movie} />
-        ))}
-    </div>
-    ) : (
-    <div className="empty">
-        <br />
-        <h2>No movies found!</h2>
-    </div>
-    )}
-    </div>
-    );
-};  
+  );
+}
 export default Content;
